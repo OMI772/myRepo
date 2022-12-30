c46ac8f56800ae099967ce89f40004ef50296a85
OMI772 <omichimanpure77@gmail.com>
2022-12-30 07:49:15 +0530

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+void quicksort(){
+	//
+}
+
+
+
+void bubblesort(){
+	//
+}
+
+
+void mergesort(){
+	//
+}
+
+void insertionsort(){
+	//
+}
+int main(){
+	cout<<"Hello World";
+	cout<<"Hello.welcome to dot batch";
+	
+}
